6cbbdd9fec661c2402078308fbaa41accd652851
Nathalie Marquesin <nmarquesin@live.com>
2017-02-23 14:35:45 +0000

    Lock buttons "-", "+" and "Go!" when timer is running

--- js/myscripts.js
+++ js/myscripts.js
@@ -2,10 +2,11 @@
 
 $(document).ready(function(){
 
-  var workMinutes = 45;
-  var timerOn = false;
+  var workMinutes = 25;
+  var paused = true;
 
   function startTimer(duration, display) {
+      paused = false;
       var timer = duration, minutes, seconds;
       var myTimer = setInterval(function () {
           minutes = parseInt(timer / 60, 10);
@@ -23,51 +24,58 @@ $(document).ready(function(){
           $("#btnStop").click(function() {
             clearInterval(myTimer);
             workMinutes = minutes + (seconds/60);
+            paused = true;
           });
 
           $("#btnReset").click(function() {
+            workMinutes = 25;
+            paused = true;
+            $("#workTime").html(formatTime());
             clearInterval(myTimer);
-            workMinutes = 45;
-            $("#workTime").html("<span id=\"time\">"+workMinutes.toString()+"</span> min");
           });
 
       }, 1000);
 
-
   }
 
-/*
-  jQuery(function ($) {
-      var fiveMinutes = 60 * 5,
-          display = $('#time');
-      startTimer(fiveMinutes, display);
-  });
-*/
-  $("#time").html(workMinutes.toString());
+
+  $("#workTime").html(formatTime());
 
   $("#btnGo").click(function() {
-    var fiveMinutes = 60 * workMinutes;
-    display = $('#workTime');
-    startTimer(fiveMinutes, display);
-
-
-
+    if (paused === true){
+      var fiveMinutes = 60 * workMinutes;
+      display = $('#workTime');
+      paused = false;
+      startTimer(fiveMinutes, display);
+    }
   });
 
   $("#btnPlusWork").click(function() {
-    if (workMinutes < 60) {
+    if (workMinutes < 60 && paused === true) {
       workMinutes += 1;
       console.log(workMinutes);
+      var countdownValue =
+      $("#workTime").html(formatTime());
     }
-    $("#time").html(workMinutes.toString());
   });
 
   $("#btnMinusWork").click(function() {
-    if (workMinutes > 5) {
+    if (workMinutes > 5 && paused === true) {
       workMinutes -= 1;
       console.log(workMinutes);
+      $("#workTime").html(formatTime());
     }
-    $("#time").html(workMinutes.toString());
   });
 
+  function formatTime() {
+    if (Math.floor((workMinutes-Math.floor(workMinutes))*60) > 9)
+    {
+      var formattedTime = Math.floor(workMinutes).toString()+":"+Math.floor((workMinutes-Math.floor(workMinutes))*60).toString();
+    }
+    else if (Math.floor((workMinutes-Math.floor(workMinutes))*60) === 0)
+    {
+        var formattedTime = Math.floor(workMinutes).toString()+":0"+ Math.floor((workMinutes-Math.floor(workMinutes))*60).toString();
+    }
+    return formattedTime;
+  }
 });
